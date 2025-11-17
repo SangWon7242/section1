ce33a23be74126611b4dd4787668230e0b0ae8bc
SangWon7242 <gotkdqja@gmail.com>
2025-11-17 15:45:20 +0900

    레이아웃 기본 적용법

---
+++ src/app/search/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <h1>서치 레이아웃</h1>
+      {children}
+    </div>
+  );
+}
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,9 +3,6 @@ export default async function Page({
 }: {
   searchParams: Promise<{ q: string }>;
 }) {
-  // ~/search?q=사과
-  // url 파라미터가 비동기로 들어옴
-  console.log(searchParams);
   const { q } = await searchParams;
 
   return <div>검색 페이지 : {q}</div>;
